0d14e6b7dc4e21e5fa3fba1f04adfa8567ec37f3
Gustavo Firmino <gustavofirmino5011@gmail.com>
2023-10-04 10:05:17 -0300

    first kotlin

---
+++ kotlin/ex01.kt
@@ -0,0 +1,46 @@
+/**
+ *
+ */
+fun main() {
+     //teste básico
+    /**/
+    val nome: String = "Gustavo"
+    val sobrenome: String = "Firmino"
+    println("Olá, $nome $sobrenome")
+    //tipo de dados
+    /*
+    * Double 64
+    * float  32
+    * Long   64
+    * Int    32
+    * Short  16
+    * Byte    8
+    * Boolean verdadeiro ou falso
+    * String  ?
+    * Char    ? */
+
+    var c: Char = 'a'
+    var s: String = "mim de papai"
+    var b: Boolean = true
+
+    var double:Double = 10.0
+    var float: Float = 10f
+    var long:Long = 10L
+    var int: Int = 10
+    var short: Short = 10
+    var byte: Byte = 10
+
+    var idade = 40
+
+    /*
+    * var para mutavel
+    * val para imultável*/
+
+    // operações
+    /*
+    * soma: +
+    * subtração: -
+    * multiplicação: *
+    * Divisão: /
+    * Resto: % */
+}
---
+++ kotlin/ex02.kt
@@ -0,0 +1,11 @@
+fun main(){
+
+    var nome: String = "Gustavo Firmino"
+    var text: String
+    val calcado: UByte = 41u
+    val pib: Long = 1869000000000
+    val pop: Int = 211000000
+    val percapita: Long = pib / pop
+    print("O valor do PIB Per Cápita brasileiro é R$ $percapita")
+
+}
